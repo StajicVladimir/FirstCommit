39631c2697611f31b8627ee0cebf72d02852eaab
StajicVladimir <vstajic@gmail.com>
2016-02-22 19:03:21 +0100

    Hello Git v2
    Druga verzija

--- FirstCommit.java
+++ FirstCommit.java
@@ -17,6 +17,7 @@ public class FirstCommit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Git!!!");
+        System.out.println("Hello Git Verzija 2");
     }
     
 }
